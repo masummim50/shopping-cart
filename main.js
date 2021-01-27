bf0adc1a170b66f257f766a91a12f677bf95e5c5
masummim50 <33899137+masummim50@users.noreply.github.com>
2021-01-27 20:51:51 +0600

    checkout option managed

--- main.js
+++ main.js
@@ -1,88 +1,89 @@
-
-const phonePrice = document.getElementById('phone-price').innerText;
-  let phonePriceNumber = parseFloat(phonePrice);
-var phoneCasePrice = document.getElementById('phone-case-price').innerText;
+const phonePrice = document.getElementById("phone-price").innerText;
+let phonePriceNumber = parseFloat(phonePrice);
+var phoneCasePrice = document.getElementById("phone-case-price").innerText;
 var phoneCasePriceNumber = parseFloat(phoneCasePrice);
 
-
-
-
-
 // phone plus click event
-const phonePlus = document.getElementById('phone-plus');
-phonePlus.addEventListener('click', function(){
-
-  cartProduct('#phone-quantity', 'phone-price', 1, phonePriceNumber);
-
+const phonePlus = document.getElementById("phone-plus");
+phonePlus.addEventListener("click", function () {
+  cartProduct("#phone-quantity", "phone-price", 1, phonePriceNumber);
 });
 
 // phone minus click event
 
-var phoneMinus = document.getElementById('phone-minus');
+var phoneMinus = document.getElementById("phone-minus");
 
-phoneMinus.addEventListener('click', function(){
-
+phoneMinus.addEventListener("click", function () {
   // cartProduct("#phone-quantity", 'phone-price', -1, phonePriceNumber);
 
-// minus button is staying disabled 
+  // minus button is staying disabled
 
-
-  if(parseFloat(document.querySelector('#phone-quantity').getAttribute('value')) < 1){
-    return false
+  if (parseFloat(document.querySelector("#phone-quantity").getAttribute("value")) < 1) {
+    return false;
   }
-  else{
-    cartProduct("#phone-quantity", 'phone-price', -1, phonePriceNumber);
+  else {
+    cartProduct("#phone-quantity", "phone-price", -1, phonePriceNumber);
   }
-  
-
 });
 
 // phone case plus click event
-var phoneCasePlus = document.getElementById('phone-case-plus');
-phoneCasePlus.addEventListener('click', function(){
-  cartProduct('#phone-case-quantity', 'phone-case-price', 1, phoneCasePriceNumber );
+var phoneCasePlus = document.getElementById("phone-case-plus");
+phoneCasePlus.addEventListener("click", function () {
+  cartProduct("#phone-case-quantity", "phone-case-price", 1, phoneCasePriceNumber);
 });
 
 // phone case minus click eveny
-var phoneCaseMinus = document.getElementById('phone-case-minus');
-phoneCaseMinus.addEventListener('click', function(){
-  if(parseFloat(document.querySelector('#phone-case-quantity').getAttribute('value')) < 1){
-    return false
+var phoneCaseMinus = document.getElementById("phone-case-minus");
+phoneCaseMinus.addEventListener("click", function () {
+  if (parseFloat(document.querySelector("#phone-case-quantity").getAttribute("value")) < 1) {
+    return false;
   }
-  else{
-  cartProduct('#phone-case-quantity', 'phone-case-price', -1, phoneCasePriceNumber);
+  else {
+    cartProduct("#phone-case-quantity", "phone-case-price", -1, phoneCasePriceNumber);
   }
 });
 
 // function for adding or removing click and updating price
-function cartProduct(id, priceid, quantityvalue, productpricenumber){
+function cartProduct(id, priceid, quantityvalue, productpricenumber) {
   let productQuantity = document.querySelector(id);
-  let value = parseFloat(productQuantity.getAttribute('value'));
-  
+  let value = parseFloat(productQuantity.getAttribute("value"));
+
   value = value + quantityvalue;
-  productQuantity.setAttribute('value', value);
-  
+  productQuantity.setAttribute("value", value);
+
   let newprice = productpricenumber * value;
   document.getElementById(priceid).innerText = newprice;
 
   //update subtotal
 
-  var subtotal = document.getElementById('subtotal').innerText;
-var subtotalNumber = parseFloat(subtotal);
-subtotalNumber = subtotalNumber + productpricenumber * quantityvalue;
-document.getElementById('subtotal').innerText = subtotalNumber;
-  
-// update tax:
-var tax = document.getElementById('tax').innerText;
-var taxNumber = parseFloat(tax);
-taxNumber = subtotalNumber *15/100;
-document.getElementById('tax').innerText = taxNumber;
+  var subtotal = document.getElementById("subtotal").innerText;
+  var subtotalNumber = parseFloat(subtotal);
+  subtotalNumber = subtotalNumber + productpricenumber * quantityvalue;
+  document.getElementById("subtotal").innerText = subtotalNumber;
 
-// show total with taxNumber
-var total = document.getElementById('total').innerText;
-var totalNumber = parseFloat(total);
-totalNumber = subtotalNumber+taxNumber;
-document.getElementById('total').innerText = totalNumber;
+  // update tax:
+  var tax = document.getElementById("tax").innerText;
+  var taxNumber = parseFloat(tax);
+  taxNumber = (subtotalNumber * 15) / 100;
+  document.getElementById("tax").innerText = taxNumber;
 
-};
+  // show total with taxNumber
+  var total = document.getElementById("total").innerText;
+  var totalNumber = parseFloat(total);
+  totalNumber = subtotalNumber + taxNumber;
+  document.getElementById("total").innerText = totalNumber;
+}
 
+// manage checkout button
+
+var checkOutButton = document.getElementById("check-out-button");
+checkOutButton.addEventListener("click", function () {
+  var total = document.getElementById("total").innerText;
+  var totalNumber = parseFloat(total);
+  if (totalNumber == 0) {
+    alert("Can't checkout empty cart");
+  }
+  else {
+  return true;
+  }
+});
